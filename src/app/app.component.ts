0977760e8491878ebc6d267af377843e0d3590d9
Millimono <millimono64.sm@gmail.com>
2024-05-08 13:38:27 +0100

    <Add> : <first update> <ajout de tous les fichiers>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { personnes } from './list_Personne';
+import { Personne } from './personne';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'OdcFormationCodeEvolutive';
+
+  etudiants: Personne[] = personnes;
+
+  ngOnInit(): void {
+    console.table(this.etudiants);
+    this.SelectStdent(this.etudiants[0].firstName);
+  }
+
+  SelectStdent(StudentsName: string) {
+    console.log(`Vous avez Mr ${StudentsName}`);
+  }
+
+  // ngOnChanges(): void {
+  //   console.log('AppComponent: ngOnChanges');
+  // }
+
+  // ngDoCheck() {
+  //   // Called during every change detection run.
+  //   console.log('AppComponent: DoCheck');
+  // }
+
+  // ngAfterContentInit() {
+  //   // Called after content (ng-content) has been projected into view.
+  //   console.log('AppComponent: AfterContentInit');
+  // }
+
+  // ngAfterContentChecked() {
+  //   // Called after every check of the component's or directive's content.
+  //   console.log('AppComponent: AfterContentChecked');
+  // }
+
+  // ngAfterViewInit() {
+  //   // Called after the component's view (and child views) has been initialized.
+  //   console.log('AppComponent: AfterViewInit');
+  // }
+
+  // ngAfterViewChecked() {
+  //   // Called after every check of the component's view (and child views).
+  //   console.log('AppComponent: AfterViewChecked');
+  // }
+
+  // ngOnDestroy(): void {
+  //   throw new Error('Method not implemented.');
+  // }
 }
---
+++ src/app/personne.ts
@@ -0,0 +1,12 @@
+export interface Personne {
+  id: number;
+  firstName: string;
+  lastName: string;
+  age: number;
+  picture: string;
+  email: string;
+  phone: string;
+  address: string;
+  created: Date;
+  categorie: string;
+}
